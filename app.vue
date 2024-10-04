744832272342e570371a14579b6a0d8577ebd280
Iwo Octoturge Strzeboński <iwo.strzebonski@gmail.com>
2024-10-04 18:49:04 +0200

    quotes fix

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 import { initFlowbite } from 'flowbite'
 
 const { $pwa } = useNuxtApp()
