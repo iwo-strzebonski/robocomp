cc216ce8d919c92fa48759b52daa53e80a0b1642
Iwo Octoturge Strzeboński <iwo.strzebonski@gmail.com>
2024-10-07 10:25:03 +0200

    Added social media links

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -13,6 +13,18 @@
     </span>
 
     <span class="inline-flex gap-4 sm:gap-8">
+      <NuxtLink to="https://fb.com/festiwal.robocomp" class="cursor-pointer">
+        <span class="sr-only">Facebook</span>
+        <lazy-client-only>
+          <fa-icon icon="fa-brands fa-square-facebook" class="h-6 w-6" />
+        </lazy-client-only>
+      </NuxtLink>
+      <NuxtLink to="https://www.instagram.com/festiwal_robocomp/" class="cursor-pointer">
+        <span class="sr-only">Instagram</span>
+        <lazy-client-only>
+          <fa-icon icon="fa-brands fa-instagram" class="h-6 w-6" />
+        </lazy-client-only>
+      </NuxtLink>
       <NuxtLink to="/blog/about" class="underline cursor-pointer">O nas</NuxtLink>
       <NuxtLink to="/blog/regulations" class="underline cursor-pointer">Regulamin</NuxtLink>
       <NuxtLink to="/blog/contact" class="underline cursor-pointer">Kontakt</NuxtLink>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,5 +1,5 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { faSquareFacebook, faSquareTwitter, faMedium, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faSun } from '@fortawesome/free-regular-svg-icons'
 import { faBars, faMoon } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -7,7 +7,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
-library.add(faSquareFacebook, faSquareTwitter, faMedium, faDiscord, faSun, faMoon, faBars)
+// Brand icons
+library.add(faSquareFacebook, faInstagram)
+// Regular icons
+library.add(faSun)
+// Solid icons
+library.add(faMoon, faBars)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('fa-icon', FontAwesomeIcon)
